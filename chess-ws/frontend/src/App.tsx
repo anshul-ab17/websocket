bcb0b51162ecd86239eca8eacd3b1bce085fa163
Anshul Bharat <anshul.ab17x@gmail.com>
2025-11-22 12:32:24 +0530

    update frontend setup

--- chess-ws/frontend/src/App.tsx
+++ chess-ws/frontend/src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
-import "./App.css";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Landing } from './pages/Landing';
+import { Game } from './pages/Game';
 
 function App() {
-  const [count , setCount] = useState(0);
- 
 
   return (
-    <div className="h-screen w-screen bg-black flex flex-col">
-        <button> 
-          JOin Room
-        </button>
+    <div className="p-4"> 
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/game' element={<Game/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
